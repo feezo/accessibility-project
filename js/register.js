77a847e7471276ba8790c2096a4f057c122f5c59
feezo <jimohafis@yahoo.co.uk>
2018-10-04 11:41:56 +0100

    edited my service worker register js file

--- js/register.js
+++ js/register.js
@@ -1,10 +1,11 @@
-// set up service worker
-if ("serviceWorker" in navigator){
-  navigator.serviceWorker.register("/sw.js" , {scope: "/" })
+/* Set up service worker */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('/sw.js', {scope: "/"})
   .then(reg => {
-    console.log("service worker registration is successful: " + reg.scope);
+    console.log('Service Worker Registration Successful: ' + reg.scope);
   })
   .catch(error => {
-    console.log("registration failed: " + error);
-  })
+    console.log('Service Worker Registration Failed: ' + error);
+  });
 }
